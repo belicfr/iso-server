3e9f0c3c8d2707c8528894bcc08ef57457d6d788
belicfr <96206878+belicfr@users.noreply.github.com>
2025-05-11 18:47:30 -0400

    chore: Program.cs

--- src/API/Program.cs
+++ src/API/Program.cs
@@ -1,16 +1,53 @@
+using System.Text;
 using Iso.Data.DbContexts;
+using Iso.Data.Managers;
+using Iso.Data.Models.RoomModel;
+using Iso.Data.Models.UserModel;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
-var builder = WebApplication.CreateBuilder(args);
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
 builder.Services.AddDbContext<AuthDbContext>(
     options => options.UseNpgsql(builder.Configuration.GetConnectionString("DbConnectionString")));
 
-builder.Services.AddIdentityApiEndpoints<IdentityUser>()
+builder.Services.AddDbContext<GameDbContext>(
+    options => options.UseNpgsql(builder.Configuration.GetConnectionString("DbConnectionString")));
+
+builder.Services.AddIdentityApiEndpoints<User>()
     .AddEntityFrameworkStores<AuthDbContext>();
+
+builder.Configuration.AddKeyPerFile(
+    builder.Configuration["ResourcesDirectory"]!);
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = builder.Configuration["JwtIssuer"],
+            ValidAudience = builder.Configuration["JwtIssuer"],
+            IssuerSigningKey = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(builder.Configuration["JWTSigningKey"]!))
+        };
+    });
+
+builder.Services.AddScoped<CompleteUserManager>();
+
+builder.Services.Configure<IdentityOptions>(options =>
+{
+    options.User.AllowedUserNameCharacters
+        = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_-";
+    options.User.RequireUniqueEmail = true;
+});
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
@@ -26,7 +63,10 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
+app.UseAuthorization();
+
 app.MapControllers();
-app.MapIdentityApi<IdentityUser>();
+// app.MapIdentityApi<User>();
 
 app.Run();
