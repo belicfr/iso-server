cac51e9d35c6affb7c718af7669efed36dac3add
belicfr <96206878+belicfr@users.noreply.github.com>
2025-05-11 18:46:43 -0400

    chore: some migrations

---
+++ src/Data/Migrations/Auth/20250511222535_Auth_001.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Iso.Data.Migrations.Auth
+{
+    /// <inheritdoc />
+    public partial class Auth_001 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ src/Data/Migrations/Auth/20250511223046_AddHomeRoomIdToUser.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Iso.Data.Migrations.Auth
+{
+    /// <inheritdoc />
+    public partial class AddHomeRoomIdToUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "HomeRoomId",
+                table: "AspNetUsers",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "HomeRoomId",
+                table: "AspNetUsers");
+        }
+    }
+}
