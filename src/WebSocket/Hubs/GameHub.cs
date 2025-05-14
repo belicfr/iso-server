beb1671b4cce1e5ef7b64b677e1a85b4f9933523
belicfr <96206878+belicfr@users.noreply.github.com>
2025-05-14 00:06:36 -0400

    refactor: split room prepare methods (public/restricted) + (sing/plur)

--- src/WebSocket/Hubs/GameHub.cs
+++ src/WebSocket/Hubs/GameHub.cs
@@ -36,40 +36,93 @@ public partial class GameHub(
     }
 
 
-    private IEnumerable<PublicNavigatorRoomResponseModel> PrepareRooms(
+    private List<PublicNavigatorRoomResponseModel> PrepareRooms(
         IEnumerable<Room> rooms)
     {
         return rooms
-            .Select(room =>
-            {
-                PublicGroupResponseModel? group = null;
-                
-                if (room.Group is not null)
-                {
-                    group = new PublicGroupResponseModel(
-                        room.Group.Id,
-                        room.Group.Name,
-                        room.Group.Description,
-                        room.Group.OwnerId,
-                        (int) room.Group.GroupMode,
-                        room.Group.RoomId,
-                        room.Group.CreatedAt);
-                }
-                
-                return new PublicNavigatorRoomResponseModel(
-                    room.Id,
-                    room.Name,
-                    room.Description,
-                    roomRuntimeService.GetPlayers(room.Id).Count,
-                    room.PlayersLimit,
-                    room.Template,
-                    group,
-                    room.OwnerId,
-                    room.Tags,
-                    room.IsPublic);
-            });
+            .Select(PrepareRoom)
+            .ToList();
+    }
+    
+    private async Task<List<RestrictedRoomResponseModel>> PrepareRoomsForOwner(
+        IEnumerable<Room> rooms)
+    {
+        List<RestrictedRoomResponseModel> restrictedRooms = new();
+
+        foreach (Room room in rooms)
+        {
+            restrictedRooms.Add(await PrepareRoomForOwner(room));
+        }
+        
+        return restrictedRooms;
     }
 
+    private PublicNavigatorRoomResponseModel PrepareRoom(Room room)
+    {
+        PublicGroupResponseModel? group = null;
+        
+        if (room.Group is not null)
+        {
+            group = new PublicGroupResponseModel(
+                room.Group.Id,
+                room.Group.Name,
+                room.Group.Description,
+                room.Group.OwnerId,
+                (int) room.Group.GroupMode,
+                room.Group.RoomId,
+                room.Group.CreatedAt);
+        }
+                
+        return new PublicNavigatorRoomResponseModel(
+            room.Id,
+            room.Name,
+            room.Description,
+            roomRuntimeService.GetPlayers(room.Id).Count,
+            room.PlayersLimit,
+            room.Template,
+            group,
+            room.OwnerId,
+            room.TagOne,
+            room.TagTwo,
+            room.IsPublic);
+    }
+    
+    private async Task<RestrictedRoomResponseModel> PrepareRoomForOwner(Room room)
+    {
+        PublicGroupResponseModel? group = null;
+        
+        if (room.Group is not null)
+        {
+            group = new PublicGroupResponseModel(
+                room.Group.Id,
+                room.Group.Name,
+                room.Group.Description,
+                room.Group.OwnerId,
+                (int) room.Group.GroupMode,
+                room.Group.RoomId,
+                room.Group.CreatedAt);
+        }
+
+        List<PublicAccountResponseModel> havingRights 
+            = (await roomService.GetPlayersWithRightsAsync(room.Id))
+                .Select(PrepareAccount)
+                .ToList();
+
+        return new RestrictedRoomResponseModel(
+            room.Id,
+            room.Name,
+            room.Description,
+            roomRuntimeService.GetPlayers(room.Id).Count,
+            room.PlayersLimit,
+            room.Template,
+            group,
+            room.OwnerId,
+            room.TagOne,
+            room.TagTwo,
+            room.IsPublic,
+            havingRights);
+    }
+    
     /// <summary>
     /// Retrieve SSO from Sub JWT claim.
     /// </summary>
