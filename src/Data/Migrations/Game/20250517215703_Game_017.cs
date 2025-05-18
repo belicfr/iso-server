38f1431fe74a90b1edcddb0778c9c115dfa1bb6d
belicfr <96206878+belicfr@users.noreply.github.com>
2025-05-18 16:04:30 -0400

    chore: migrations

---
+++ src/Data/Migrations/Game/20250517215703_Game_017.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Iso.Data.Migrations.Game
+{
+    /// <inheritdoc />
+    public partial class Game_017 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Promotions",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "text", nullable: false),
+                    Title = table.Column<string>(type: "text", nullable: false),
+                    ThumbnailPath = table.Column<string>(type: "text", nullable: false),
+                    Action = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Promotions", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Promotions");
+        }
+    }
+}
