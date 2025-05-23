0579ed39df2282c3f30388a47e4a50f6a10eda1d
belicfr <96206878+belicfr@users.noreply.github.com>
2025-05-23 03:40:37 -0400

    chore: migrations

---
+++ src/Data/Migrations/Game/20250520212201_Game_019.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Iso.Data.Migrations.Game
+{
+    /// <inheritdoc />
+    public partial class Game_019 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Thumbnail",
+                table: "Rooms",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Thumbnail",
+                table: "Rooms");
+        }
+    }
+}
