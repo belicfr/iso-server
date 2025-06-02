73b2d06c374f8b65414a347d5180f8e462c5d697
belicfr <96206878+belicfr@users.noreply.github.com>
2025-06-02 19:15:25 -0400

    feat: add position unmapped attribute into user model

--- src/Data/Models/UserModel/User.cs
+++ src/Data/Models/UserModel/User.cs
@@ -3,6 +3,7 @@ using System.ComponentModel.DataAnnotations.Schema;
 using Microsoft.AspNetCore.Identity;
 using Iso.Data.Models.RoomModel;
 using System.Collections.Generic;
+using Iso.Shared.Physic;
 
 namespace Iso.Data.Models.UserModel;
 
@@ -19,4 +20,7 @@ public class User: IdentityUser
     public string? HomeRoomId { get; set; }
     
     public List<User> Friends { get; set; } = new();
+
+    [NotMapped]
+    public Coord2D Position { get; set; } = new(0f, 0f);
 }
