a60068f817f3554926ade04149c41087a05f99ed
belicfr <96206878+belicfr@users.noreply.github.com>
2025-06-02 19:14:08 -0400

    feat: create Coord2D utility record

---
+++ src/Shared/Physic/Coord2D.cs
@@ -0,0 +1,10 @@
+namespace Iso.Shared.Physic;
+
+/// <summary>
+/// Represents a 2-dimensional coord.
+/// </summary>
+/// <param name="X"></param>
+/// <param name="Y"></param>
+public record Coord2D(
+    float X,
+    float Y);
