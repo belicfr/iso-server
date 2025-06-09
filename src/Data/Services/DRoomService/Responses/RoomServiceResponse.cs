903e0228bcbf2abb133711954c1cc38afcefe619
belicfr <96206878+belicfr@users.noreply.github.com>
2025-06-09 15:46:56 -0400

    chore: add missing SUCCESS and FAIL RoomServiceResponse codes

--- src/Data/Services/DRoomService/Responses/RoomServiceResponse.cs
+++ src/Data/Services/DRoomService/Responses/RoomServiceResponse.cs
@@ -2,5 +2,8 @@ namespace Iso.Data.Services.DRoomService.Responses;
 
 public enum RoomServiceResponse
 {
+    SUCCESS = ServiceResponse.SUCCESS,
+    FAIL = ServiceResponse.FAIL,
+    
     ROOM_NOT_EXISTS = 2,
 }
